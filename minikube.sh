535be7328cfdf6496d513ca1d95d703444c3a2dc
Yuxiang LIU <david-khala@hotmail.com>
2020-01-24 20:56:59 +0800

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -11,5 +11,11 @@ clean() {
 view() {
 	minikube dashboard
 }
+install(){
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    chmod +x minikube 
+    sudo cp minikube /usr/local/bin/ 
+    rm minikube
+}
 
 $1
