df6ac434caa95eadaa8c24b12b94f0779f676a65
David Liu <david-khala@hotmail.com>
2023-09-26 22:54:18 +0800

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -11,11 +11,6 @@ clean() {
 view() {
 	minikube dashboard
 }
-install(){
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-    chmod +x minikube 
-    sudo cp minikube /usr/local/bin/ 
-    rm minikube
-}
+
 
 $1
