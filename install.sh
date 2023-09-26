41d76c3c4d0c1a515c9d83877cb70df9bdb95c57
David Liu <david-khala@hotmail.com>
2023-09-26 22:18:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ toolSet() {
 	sudo apt-get install -y kubelet kubeadm kubectl
 	# sudo apt-mark hold kubelet kubeadm kubectl
 }
-
+minikube(){
+	curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+	chmod +x minikube 
+	sudo mv minikube /usr/local/bin/ 
+}
 
 $1
