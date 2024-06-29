8d5b99e3cabfe1844544ded79fc911b9a59bb2d9
David Liu <david-khala@hotmail.com>
2024-06-29 14:38:39 +0800

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -1,13 +1,13 @@
-install() {
-  sudo snap install kubectl --classic
-}
 version() {
   kubectl version --client
-
 }
 logs() {
   kubectl logs --since=1h --tail=200 $1
 }
+nodes(){
+  kubectl get nodes
+}
+
 namespaces(){
   kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
 }
