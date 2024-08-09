263c504f085416cc720112b60fe6c514b46ea241
David Liu <david-khala@hotmail.com>
2024-08-09 13:13:16 +0000

    fix typo

--- kubectl.sh
+++ kubectl.sh
@@ -4,19 +4,19 @@ version() {
 logs() {
   kubectl logs --since=1h --tail=200 $1
 }
-nodes(){
-  kubectl get nodes
+nodes() {
+  kubectl get nodes -o 'jsonpath={.items[*].metadata.name}'
+
 }
 
-namespaces(){
-  kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
+namespaces() {
+  kubectl get namespaces -o jsonpath='{.items[*].metadata.name}'
 }
-deployments(){
+deployments() {
   kubectl get deployment -n $namespace --no-headers=true | awk '{print $1}'
 }
-deployment-restart(){
-  kubectl rollout restart deployment $1 -n $namespace  
+deployment-restart() {
+  kubectl rollout restart deployment $1 -n $namespace
 }
-
 
 $@
