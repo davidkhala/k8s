9239f2f4a0f0cbf78f79a1e3cdba341405f6c248
David Liu <david-khala@hotmail.com>
2025-04-03 13:37:06 +0800

    add helm tips

--- helm/helm.sh
+++ helm/helm.sh
@@ -1,4 +1,13 @@
 list() {
   helm list --all-namespaces
 }
+install() {
+  helm install "$@" --wait --create-namespace
+}
+uninstall() {
+  helm uninstall "$@" --wait --ignore-not-found
+}
+upgrade() {
+  helm upgrade "$@" --wait
+}
 $@
